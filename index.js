4948a52b121035077bef909b46dbe1d40e212a26
Jesse SMith <jesse.lee.smith@gmail.com>
2018-01-27 20:15:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,8 +59,11 @@ function removeFromCart(item) {
     if(item === Object.keys(cart[i])[0])
     {
       cart.splice(i, 1);
+      return cart;
     }
   }
+  console.log("That item is not in your cart.");
+  return cart;
 }
 
 function placeOrder(cardNumber) {
