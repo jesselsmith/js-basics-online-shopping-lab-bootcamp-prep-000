5b5cf1bb00c8668e506d043506f1b28cb6b4fd13
Jesse SMith <jesse.lee.smith@gmail.com>
2018-01-27 20:23:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,5 +72,6 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.");
   }else{
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart.length = 0;
   }
 }
