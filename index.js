5204da3360bc9e0b09101bfae7bcbf5075a34ec0
Jesse SMith <jesse.lee.smith@gmail.com>
2018-01-27 20:17:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   for(var i = 0; i < cart.length; i++){
-    if(cart[i].hasOwnProperty)
+    if(cart[i].hasOwnProperty(item))
     {
       cart.splice(i, 1);
       return cart;
